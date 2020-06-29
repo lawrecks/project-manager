0f008dbc77db1728c5a119e0b5918898a6855c52
lawrecks <ugochukwu@winexviv.com>
2020-06-29 18:37:41 +0100

    Initial commit

---
+++ src/Project/Project.js
@@ -0,0 +1,13 @@
+import React , {Component} from 'react';
+
+class Project extends Component () {
+    render () {
+        return (
+            <div className="App">
+                this is Project Component
+            </div>
+        );
+    }
+}
+
+export default Project;
